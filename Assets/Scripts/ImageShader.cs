41c637783e39c69cde17c8b4f10f3e3f01fbe2a7
crazya02 <alekfleischer@yahoo.com>
2016-06-24 15:50:48 -0700

    Finally got it working!!

---
+++ Assets/Scripts/ImageShader.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+public class ImageShader : MonoBehaviour {
+
+	public float intensity;
+	public Shader shader;
+	private Material material;
+
+	// Creates a private material used to the effect
+	void Awake ()
+	{
+		material = new Material( shader );
+	}
+
+	// Postprocess the image
+	void OnRenderImage (RenderTexture source, RenderTexture destination)
+	{
+		if (intensity == 0)
+		{
+			Graphics.Blit (source, destination);
+			return;
+		}
+
+		material.SetFloat("_bwBlend", intensity);
+		Graphics.Blit (source, destination, material);
+	}
+}
